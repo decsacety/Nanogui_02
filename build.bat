245c737143ed3bd973db4762d9ca19e8aab4e3d8
decsacety <49853671+decsacety@users.noreply.github.com>
2021-04-11 21:34:21 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ call E:\VS2019\VC\Auxiliary\Build\vcvarsall x64
 mkdir build
 pushd  build
 
-cl /c ../src/common.cpp /MDd /I ../include /I ../ext/glfw/include /FO"nanogui.obj"
+cl /c ../src/common.cpp /MDd /I ../include /I ../ext/glfw/include /Fo"nanogui"
 cl /c ../src/screen.cpp /MDd /I ../include /I ../ext/glfw/include /I ../ext/glad/include
 cl /c ../src/glad.c /MDd /I ../ext/glad/include
 link -lib nanogui.obj screen.obj glad.obj ../libs/glfw3.lib opengl32.lib User32.lib kernel32.lib gdi32.lib winmm.lib Shell32.lib
